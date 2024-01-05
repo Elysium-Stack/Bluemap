eb03a13367fbed711ad97fe228d8ce766aaa3e7a
Jurien Hamaker <whoami@jurien.dev>
2024-01-05 16:15:35 +0100

    fix(nginx):  Entry point update for new env var

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${HUB_HOST} ${PARTYGAMES_HOST} ${SURVIVAL_HOST} ${SKYBLOCK_HOST}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${HUB_HOST} ${PARTYGAMES_HOST} ${SURVIVAL_HOST} ${SKYBLOCK_HOST} ${HARD_HOST}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
